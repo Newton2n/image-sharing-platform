1a91c9f5b089bd35a5479b34fd72a99fc5c1341e
Newton <156936866+Newton2n@users.noreply.github.com>
2025-11-01 20:42:50 +0600

    fixing case sensitive

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Header from './Header/Header';
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
-import Input from "./input";
+import Input from "./Input";
 import Select from "./Select";
 import PostCard from "./PostCard";
 import Login from "./Login";
