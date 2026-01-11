27f64b6069d762c0739c8df83935d9193aab9fcf
Newton <156936866+Newton2n@users.noreply.github.com>
2026-01-12 00:09:39 +0600

    fix:fix some tailwind property

--- src/components/ui/author-picture.jsx
+++ src/components/ui/author-picture.jsx
@@ -3,27 +3,26 @@ import Image from "next/image";
 async function AuthorPicture({ userId, className }) {
   //author profile response
   const authorInformation = await service.getProfileInformationQuery(userId);
-
+  
   //author profile picture response
   const authorProfileImgUrlResponse = await service.fileView(
     authorInformation.rows[0].profileImageId
   );
 
   //final img url
-  const authorProfileImgUrl = authorInformation.rows[0].profileImageId
+  const authorProfileImgUrl = authorInformation?.rows[0]?.profileImageId
     ? authorProfileImgUrlResponse
     : "/image/initial-profile-pic2.webp";
 
   return (
     <span
-      className={`${className} relative rounded-full overflow-hidden shadow-xl/30 `}
+      className={`${className} relative rounded-full overflow-hidden  `}
     >
       <Image
         src={authorProfileImgUrl}
-        height={100}
-        width={100}
-        alt="profilePic"
-        className="object-cover h-full w-full  "
+        fill
+        alt={`${authorInformation?.rows[0]?.fullName} profile picture`}
+        className="object-cover"
       />
     </span>
   );
