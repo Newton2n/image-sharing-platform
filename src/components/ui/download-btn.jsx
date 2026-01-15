ba46bdd7b1e10d2a0305eb9433d0c7860c96733f
Newton <156936866+Newton2n@users.noreply.github.com>
2026-01-15 19:53:24 +0600

    fix:add stopPropagation in download btn

--- src/components/ui/download-btn.jsx
+++ src/components/ui/download-btn.jsx
@@ -5,10 +5,10 @@ import service from "@/lib/appwrite/config";
 import { Button } from "..";
 function DownloadBtn({ featuredImg }) {
   return (
- 
     <button
       className=" bg-white hover:bg-gray-200 p-1  min-[300px]:p-1.5 min-[500px]:p-2  rounded-md cursor-pointer"
       onClick={(e) => {
+        e.stopPropagation();
         e.preventDefault();
         service.fileDownload(featuredImg);
       }}
