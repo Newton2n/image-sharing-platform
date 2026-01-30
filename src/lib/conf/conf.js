065e1e4173e201842d2b24e0b226a40aec3ce099
Newton <156936866+Newton2n@users.noreply.github.com>
2026-01-30 16:30:35 +0600

    fix(config):remove appwrite api key from config.js

--- src/lib/conf/conf.js
+++ src/lib/conf/conf.js
@@ -1,12 +1,12 @@
-
 const conf = {
-    appwriteUrl: String(process.env.NEXT_PUBLIC_APPWRITE_URL),
-    appwriteProjectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: String(process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID),
-    appwriteTableId: String(process.env.NEXT_PUBLIC_APPWRITE_TABLE_ID),
-    appwriteUsersProfileInformationTableId: String(process.env.NEXT_PUBLIC_APPWRITE_USERS_PROFILE_INFORMATION_TABLE_ID),
-    appwriteBucketId: String(process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID),
-    appwriteApiKey:String(process.env.NEXT_PUBLIC_APPWRITE_API_KEY)
+  appwriteUrl: String(process.env.NEXT_PUBLIC_APPWRITE_URL),
+  appwriteProjectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID),
+  appwriteTableId: String(process.env.NEXT_PUBLIC_APPWRITE_TABLE_ID),
+  appwriteUsersProfileInformationTableId: String(
+    process.env.NEXT_PUBLIC_APPWRITE_USERS_PROFILE_INFORMATION_TABLE_ID,
+  ),
+  appwriteBucketId: String(process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID),
 };
 
 export default conf;
